62792fc9319e9426f356567d60b063bc378e3062
Ethan Siegel <ewsiegel@gmail.com>
2023-08-07 23:23:23 -0500

    updates to collections_to_hub

--- aap-integration-tools/collections/collections_to_hub.bash
+++ aap-integration-tools/collections/collections_to_hub.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+[ -z "$GALAXY_NG_USER" ] && echo "GALAXY_NG_USER is not set"
+[ -z "$GALAXY_NG_PASSWORD" ] && echo "GALAXY_NG_PASSWORD is not set"
+[ -z "$GALAXY_NG_URL" ] && echo "GALAXY_NG_URL is not set"
+[ ! -f "/opt/galaxy_token" ] && echo "Expected galaxy token at /opt/galaxy_token"
+
 DIR=~/ansible-collections
 
 for path in ${DIR}/*.tar.gz; do
@@ -12,7 +17,7 @@ for path in ${DIR}/*.tar.gz; do
     #create namespace
     curl -sku ${GALAXY_NG_USER}:"${GALAXY_NG_PASSWORD}" -X POST -H 'Content-Type: application/json' -d '{"name":"'${NAMESPACE}'","groups":[]}' ${GALAXY_NG_URL}/api/galaxy/_ui/v1/namespaces/ | jq
     #publish collection
-    ansible-galaxy collection publish "${DIR}/${NAMESPACE}-${COLLECTION}-${VERSION}.tar.gz" -s ${GALAXY_NG_URL} --token $(cat /opt/galaxy_token) --ignore-certs
+    ansible-galaxy collection publish "${DIR}/${NAMESPACE}-${COLLECTION}-${VERSION}.tar.gz" -s ${GALAXY_NG_URL}/api/galaxy/ --token $(cat /opt/galaxy_token) --ignore-certs
     #approve collection
     curl -sku ${GALAXY_NG_USER}:"${GALAXY_NG_PASSWORD}" -X POST -H 'Content-Type: application/json' "${GALAXY_NG_URL}"/api/galaxy/v3/collections/"${NAMESPACE}"/"${COLLECTION}"/versions/"${VERSION}"/move/staging/published/ | jq
 done
