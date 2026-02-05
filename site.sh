8930cf47eb0a31a2f372975f5e6aa1a1d99eba7b
Mario Vazquez <mavazque@redhat.com>
2026-02-05 10:24:43 +0100

    Fixed generator and issue with rendering of cm
    Signed-off-by: Mario Vazquez <mavazque@redhat.com>

--- site.sh
+++ site.sh
@@ -2,5 +2,5 @@
 
 _CURR_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 rm -rf $_CURR_DIR/gh-pages $_CURR_DIR/.cache
-antora site.yml
-#gulp
+
+podman run --rm -ti -v $_CURR_DIR:/antora:z --entrypoint antora docker.io/antora/antora:3.0.0 site.yml
